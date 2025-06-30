d2fc8484b2d970841dc6939089f0203be1fd56ce
willkl2415 <68011693+willkl2415@users.noreply.github.com>
2025-06-30 08:52:40 +0100

    app.py

--- app.py
+++ app.py
@@ -1,32 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
-import json
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
-
-@app.route('/shop')
-def shop():
-    return render_template('shop.html')
-
-@app.route('/rewards')
-def rewards():
-    with open('rewards_data.json') as f:
-        data = json.load(f)
-    return render_template('rewards.html', data=data)
-
-@app.route('/scan', methods=['GET', 'POST'])
-def scan():
-    if request.method == 'POST':
-        code = request.form['qr_code']
-        return redirect(url_for('rewards'))
-    return render_template('scan.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
